f448d7ccdc9429301fa8597d94b9d1a4d4777628
devilcrax <craxdevil7@gmail.com>
2020-08-28 13:15:12 +0000

    added access

--- test.tf
+++ test.tf
@@ -11,6 +11,33 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_iam_user" "circleci" {
+  name = var.user
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "circleci" {
+  user = aws_iam_user.circleci.name
+}
+
+data "template_file" "circleci_policy" {
+  template = file("circleci_s3_access.tpl.json")
+  vars = {
+    instance_arn = aws_instance.example.arn
+  }
+}
+
+resource "local_file" "circle_credentials" {
+  filename = "tmp/circleci_credentials"
+  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
+}
+
+resource "aws_iam_user_policy" "circleci" {
+  name   = "AllowCircleCI"
+  user   = aws_iam_user.circleci.name
+  policy = data.template_file.circleci_policy.rendered
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
